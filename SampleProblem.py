ddbc869431c31385bc828e302f952a3f70c6bc53
samLohnes <sam_lohnes@icloud.com>
2020-10-22 22:31:55 -0400

    commit for sampleProblem from dailyCodingProblems.com

--- SampleProblem.py
+++ SampleProblem.py
@@ -4,7 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 def main():
     x = [3,5]
-    print(binStepper(2, x))
+    print(binStepper(10, x))
 
 def binStepper(steps, choices):
     # Steps = The number of steps left to take

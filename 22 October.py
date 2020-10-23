87545576681b82162685be27ea01dc5d7b5b0be0
samLohnes <73322997+samLohnes@users.noreply.github.com>
2020-10-22 22:28:43 -0400

    Add files via upload

---
+++ 22 October.py
@@ -0,0 +1,16 @@
+def main():
+    x = [10,15,12]
+    y = 17
+
+    print(runThrough(x, y))
+
+def runThrough(set, outsider):
+    for i in set:
+        if outsider - i in set:
+            return True
+    return False
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ SampleProblem.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    x = [3,5]
+    print(binStepper(2, x))
+
+def binStepper(steps, choices):
+    # Steps = The number of steps left to take
+    # The choices that are possible
+    work = 0
+    if (steps > 0):
+        for i in choices:
+            if steps - i == 0:
+                work+=1
+            elif steps - i > 0:
+                work += binStepper(steps - i, choices)
+
+    return work
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
